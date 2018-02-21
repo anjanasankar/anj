e6ec1abf1cd418b8e737522845db0aea1a50c65e
anjanasankar <36299372+anjanasankar@users.noreply.github.com>
2018-02-21 20:05:18 +0530

    Vocon

---
+++ Vowels.py
@@ -0,0 +1,5 @@
+Ch = input("Enter a character: ")
+If (ch=='A' or ch=='a' or ch=='E' or ch=='e' or ch=='I' or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'
+print(ch, "is a Vowel")
+else:
+print(ch, "is a consonant)
