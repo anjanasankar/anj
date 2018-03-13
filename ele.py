fd9c79f43083accd9284735e703c84830ebc1421
anjanasankar <36299372+anjanasankar@users.noreply.github.com>
2018-03-13 17:05:17 +0530

    Update ele.py

--- ele.py
+++ ele.py
@@ -1,7 +1,8 @@
-a=[]
-n=int(input("Enter number of elements:"))
-for i in range(1,n+1):
-b=int(input("Enter element:"))
-a.append(b)
-a.sort()
-print("Largest element is:",a[n-1])
+def max_num_in_list( list ):
+max = list[ 0 ]
+for a in list:
+if a > max:
+max = a
+return max
+print(max_num_in_list([1, 2, -8, 0]))
+
