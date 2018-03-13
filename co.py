f3e81036df0cc28287c24a8db1fff05f51a0c0db
anjanasankar <36299372+anjanasankar@users.noreply.github.com>
2018-03-13 17:21:23 +0530

    co

---
+++ co.py
@@ -0,0 +1,7 @@
+data_list = [-5, -23, 5, 0, 23, -6, 23, 67]
+new_list = []
+minimum = data_list[0]  # arbitrary number in list 
+for x in data_list: 
+if x < minimum:
+minimum = value
+new_list.append(i)
