eb82e842c6aba767d2ccfaf58c017c5318e489ff
anjanasankar <36299372+anjanasankar@users.noreply.github.com>
2018-03-14 19:33:55 +0530

    swapp

---
+++ swapp.py
@@ -0,0 +1,16 @@
+while True:
+print("Enter 'x' for exit.")
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+if num1 == 'x':	
+else:
+		number1 = int(num1)
+		number2 = int(num2)
+		swap = number1
+		number1 = number2
+		number2 = swap
+		print("Value of First and Second number after swapping:")
+		print("First Number =",number1,"\nSecond Number=",number2,"\n"
+    
+    
+    
